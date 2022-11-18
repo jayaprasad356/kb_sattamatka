a89d7cd8fc1aeb774c3420696ff73497a5177fa7
jayaprasad356 <jayaprasad356@gmail.com>
2022-11-18 20:32:06 +0530

    crud changed

--- includes/crud.php
+++ includes/crud.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('DOMAIN_URL', 'https://dbboss.greymatterworks.in'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
+define('DOMAIN_URL', 'https://admin.dpbossmatka.info/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
 define('ALLOW_MODIFICATION', 1);
 define('JWT_SECRET_KEY', '12345678'); /* chnage to your secret key here set it to anything you like*/
 
@@ -15,9 +15,9 @@ class Database
      */
 
     private $db_host = "localhost";  // Change as required
-    private $db_user = "u743445510_dbboss";  // Change as required
-    private $db_pass = "Dbboss@2022";  // Change as required
-    private $db_name = "u743445510_dbboss";    // Change as required
+    private $db_user = "u713985783_dpboss";  // Change as required
+    private $db_pass = "Dpboss@2022";  // Change as required
+    private $db_name = "u713985783_dpboss";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
